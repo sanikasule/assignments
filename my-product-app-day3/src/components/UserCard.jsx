b35e0ec7b7cd415f15923fd270832bc167471df8
sanikasule <sulesanika25@gmail.com>
2026-02-18 21:13:21 +0530

    Added day3 project with changes

---
+++ my-product-app-day3/src/components/UserCard.jsx
@@ -0,0 +1,60 @@
+function UserCard({ user, onViewUsers }) {
+  return (
+    <div style={{
+      border: '1px solid #ddd',
+      borderRadius: '8px',
+      padding: '15px',
+      background: 'white',
+      cursor: 'pointer',
+      transition: 'transform 0.2s, box-shadow 0.2s'
+    }}
+    onMouseOver={(e) => {
+      e.currentTarget.style.transform = 'translateY(-5px)';
+      e.currentTarget.style.boxShadow = '0 4px 12px rgba(0,0,0,0.15)';
+    }}
+    onMouseOut={(e) => {
+      e.currentTarget.style.transform = 'translateY(0)';
+      e.currentTarget.style.boxShadow = 'none';
+    }}
+
+    onClick={() => onViewUsers(user.id)}
+    >
+      
+      <h3 style={{
+        fontSize: '17px',
+        margin: '0 0 10px 0',
+        height: '30px',
+        overflow: 'hidden'
+      }}>
+        User Id: {user.id}
+      </h3>
+      <h3 style={{
+        fontSize: '17px',
+        margin: '0 0 10px 0',
+        height: '30px',
+        overflow: 'hidden'
+      }}>
+        UserName: {user.username}
+      </h3>
+      <h3 style={{
+        fontSize: '17px',
+        margin: '0 0 10px 0',
+        height: '50px',
+        overflow: 'hidden'
+      }}>
+        Email: {user.email}
+      </h3>
+      <h3 style={{
+        fontSize: '17px',
+        margin: '0 0 10px 0',
+        height: '30px',
+        overflow: 'hidden'
+      }}>
+        Password: {user.password}
+      </h3>
+      
+    </div>
+  );
+}
+
+export default UserCard;
