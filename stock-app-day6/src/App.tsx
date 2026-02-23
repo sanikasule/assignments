0407b2e8411656d35f44e903f367bea76791ca78
sanikasule <sulesanika25@gmail.com>
2026-02-23 12:29:47 +0530

    initial app building

--- stock-app-day6/src/App.tsx
+++ stock-app-day6/src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import StockCard from './components/StockCard'
+import DataTable from './components/dataTable'
+import type { Stock } from './types/stock.types'
+import { stocks } from './data/stockData'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Stock trading app</h1>
+      <StockCard stock={stocks[0]}/>
+      <br />
+      <DataTable<Stock> data={stocks}
+        rowKey='id'
+        columns={[
+          {key: 'symbol', header: 'Symbol'},
+          {key: 'price', header: 'Price', render: (val) => `$${Number(val).toFixed(2)}`},
+          {key: 'changePct', header: 'Change %', render: (val) => {
+            const n = Number(val);
+            return <span style={{color: n>=0 ? 'green' : 'red'}}>
+              {n>=0 ? '+' : ''}{n.toFixed(2)}%
+            </span>
+          }},
+        ]}
+        />
+    </div>
   )
 }
 
